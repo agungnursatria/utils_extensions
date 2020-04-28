8c8f4902ae3d95258c9cc326f9c35df6967bd61c
Agung Nursatria <agungnursatria@gmail.com>
2020-04-28 10:12:36 +0700

    Add function

---
+++ lib/src/extension/set/parseExt.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+extension ListParse on List {
+  /// Transform list to json
+  String toJsonString() => jsonEncode(this);
+
+  /// Get total value of list of num (int/double)
+  /// Example: [1,2,3,4] => 10
+  num valueTotal() {
+    num total = 0;
+    if (this == null || this.isEmpty) return total;
+    if (this[0] is num) for (var v in this) total += v;
+    throw FormatException('Can only accepting set of num (int/double)');
+  }
+}
